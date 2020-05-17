4b4dac82da37afd41ddd6500db74289c51ddda99
Gru <pcbastion@gmail.com>
2020-05-17 15:44:30 +0200

    V2: Meshes with over 64k verts work, commented and tidied

--- MeshCombineWizard.cs
+++ MeshCombineWizard.cs
@@ -1,75 +1,121 @@
 using UnityEngine;
 using UnityEditor;
 using System.Collections.Generic;
+using UnityEngine.Rendering;
 
-public class MeshCombineWizard : ScriptableWizard {
-    public GameObject parentOfObjectsToCombine;
+public class MeshCombineWizard : ScriptableWizard
+{
+	public GameObject combineParent;
 
-    [MenuItem("E.S. Tools/Mesh Combine Wizard")]
-    static void CreateWizard() {
-        ScriptableWizard.DisplayWizard<MeshCombineWizard>("Mesh Combine Wizard");
-    }
+	[MenuItem("E.S. Tools/Mesh Combine Wizard")]
+	static void CreateWizard()
+	{
+		var wizard = DisplayWizard<MeshCombineWizard>("Mesh Combine Wizard");
 
-    void OnWizardCreate() {
-        if(parentOfObjectsToCombine == null) return;
+		// If there is selection, and the selection of one Scene object, auto-assign it
+		var selectionObjects = Selection.objects;
+		if (selectionObjects != null && selectionObjects.Length == 1) {
+			var firstSelection = selectionObjects[0] as GameObject;
+			if (firstSelection != null) {
+				wizard.combineParent = firstSelection;
+			}
+		}
+	}
 
-        Vector3 originalPosition = parentOfObjectsToCombine.transform.position;
-        parentOfObjectsToCombine.transform.position = Vector3.zero;
+	void OnWizardCreate()
+	{
+		// Verify there is existing object root, ptherwise bail.
+		if (combineParent == null) {
+			Debug.LogError("Mesh Combine Wizard: Parent of objects to combne not assigned. Operation cancelled.");
+			return;
+		}
 
-        MeshFilter[] meshFilters = parentOfObjectsToCombine.GetComponentsInChildren<MeshFilter>();
-        Dictionary<Material, List<MeshFilter>> materialToMeshFilterList = new Dictionary<Material, List<MeshFilter>>();
-        List<GameObject> combinedObjects = new List<GameObject>();
+		// Remember the original position of the object. 
+		// For the operation to work, the position must be temporarily set to (0,0,0).
+		Vector3 originalPosition = combineParent.transform.position;
+		combineParent.transform.position = Vector3.zero;
 
-        for(int i = 0; i < meshFilters.Length; i++) {
-            var materials = meshFilters[i].GetComponent<MeshRenderer>().sharedMaterials;
-            if(materials == null) continue;
-            if(materials.Length > 1) {
-                parentOfObjectsToCombine.transform.position = originalPosition;
-                Debug.LogError("Objects with multiple materials on the same mesh are not supported. Create multiple meshes from this object's sub-meshes in an external 3D tool and assign separate materials to each.");
-                return;
-            }
-            var material = materials[0];
-            if(materialToMeshFilterList.ContainsKey(material)) materialToMeshFilterList[material].Add(meshFilters[i]);
-            else materialToMeshFilterList.Add(material, new List<MeshFilter>() { meshFilters[i] });
-        }
+		// Locals
+		Dictionary<Material, List<MeshFilter>> materialToMeshFilterList = new Dictionary<Material, List<MeshFilter>>();
+		List<GameObject> combinedObjects = new List<GameObject>();
 
-        foreach(var entry in materialToMeshFilterList) {
-            List<MeshFilter> meshesWithSameMaterial = entry.Value;
-            string materialName = entry.Key.ToString().Split(' ')[0];
+		MeshFilter[] meshFilters = combineParent.GetComponentsInChildren<MeshFilter>();
 
-            CombineInstance[] combine = new CombineInstance[meshesWithSameMaterial.Count];
-            for(int i = 0; i < meshesWithSameMaterial.Count; i++) {
-                combine[i].mesh = meshesWithSameMaterial[i].sharedMesh;
-                combine[i].transform = meshesWithSameMaterial[i].transform.localToWorldMatrix;
-            }
+		// Go through all mesh filters and establish the mapping between the materials and all mesh filters using it.
+		foreach (var meshFilter in meshFilters) {
+			var meshRenderer = meshFilter.GetComponent<MeshRenderer>();
+			if (meshRenderer == null) {
+				Debug.LogWarning("The Mesh Filter on object " + meshFilter.name + " has no Mesh Renderer component attached. Skipping.");
+				continue;
+			}
+			
+			var materials = meshRenderer.sharedMaterials;
+			if (materials == null) {
+				Debug.LogWarning("The Mesh Renderer on object " + meshFilter.name + " has no material assigned. Skipping.");
+				continue;
+			}
 
-            Mesh combinedMesh = new Mesh();
-            combinedMesh.CombineMeshes(combine);
-            materialName += "_" + combinedMesh.GetInstanceID();
-            AssetDatabase.CreateAsset(combinedMesh, "Assets/CombinedMeshes_" + materialName + ".asset");
+			// If there are multiple materials on a single mesh, cancel.
+			if (materials.Length > 1) {
+				// Rollback: return the object to original position
+				combineParent.transform.position = originalPosition;
+				Debug.LogError("Objects with multiple materials on the same mesh are not supported. Create multiple meshes from this object's sub-meshes in an external 3D tool and assign separate materials to each. Operation cancelled.");
+				return;
+			}
+			var material = materials[0];
 
-            string goName = (materialToMeshFilterList.Count > 1)? "CombinedMeshes_" + materialName : "CombinedMeshes_" + parentOfObjectsToCombine.name;
-            GameObject combinedObject = new GameObject(goName);
-            var filter = combinedObject.AddComponent<MeshFilter>();
-            filter.sharedMesh = combinedMesh;
-            var renderer = combinedObject.AddComponent<MeshRenderer>();
-            renderer.sharedMaterial = entry.Key;
-            combinedObjects.Add(combinedObject);
-        }
+			// Add material to mesh filter mapping to dictionary
+			if (materialToMeshFilterList.ContainsKey(material)) materialToMeshFilterList[material].Add(meshFilter);
+			else materialToMeshFilterList.Add(material, new List<MeshFilter>() { meshFilter });
+		}
 
-        GameObject resultGO = null;
-        if(combinedObjects.Count > 1) {
-            resultGO = new GameObject("CombinedMeshes_" + parentOfObjectsToCombine.name);
-            foreach(var combinedObject in combinedObjects) combinedObject.transform.parent = resultGO.transform;
-        } else {
-            resultGO = combinedObjects[0];
-        }
+		// For each material, create a new merged object, in the scene and in the assets folder.
+		foreach (var entry in materialToMeshFilterList) {
+			List<MeshFilter> meshesWithSameMaterial = entry.Value;
+			// Create a convenient material name
+			string materialName = entry.Key.ToString().Split(' ')[0];
 
-        Object prefab = PrefabUtility.CreateEmptyPrefab("Assets/" + resultGO.name + ".prefab");
-        PrefabUtility.ReplacePrefab(resultGO, prefab, ReplacePrefabOptions.ConnectToPrefab);
+			CombineInstance[] combine = new CombineInstance[meshesWithSameMaterial.Count];
+			for (int i = 0; i < meshesWithSameMaterial.Count; i++) {
+				combine[i].mesh = meshesWithSameMaterial[i].sharedMesh;
+				combine[i].transform = meshesWithSameMaterial[i].transform.localToWorldMatrix;
+			}
 
-        parentOfObjectsToCombine.SetActive(false);
-        parentOfObjectsToCombine.transform.position = originalPosition;
-        resultGO.transform.position = originalPosition;
-    }
+			// Create a new mesh using the combined properties
+			Mesh combinedMesh = new Mesh { indexFormat = IndexFormat.UInt32 };
+			combinedMesh.CombineMeshes(combine);
+
+			// Create asset
+			materialName += "_" + combinedMesh.GetInstanceID();
+			AssetDatabase.CreateAsset(combinedMesh, "Assets/CombinedMeshes_" + materialName + ".asset");
+
+			// Create game object
+			string goName = (materialToMeshFilterList.Count > 1)? "CombinedMeshes_" + materialName : "CombinedMeshes_" + combineParent.name;
+			GameObject combinedObject = new GameObject(goName);
+			var filter = combinedObject.AddComponent<MeshFilter>();
+			filter.sharedMesh = combinedMesh;
+			var renderer = combinedObject.AddComponent<MeshRenderer>();
+			renderer.sharedMaterial = entry.Key;
+			combinedObjects.Add(combinedObject);
+		}
+
+		// If there were more than one material, and thus multiple GOs created, parent them and work with result
+		GameObject resultGO = null;
+		if (combinedObjects.Count > 1) {
+			resultGO = new GameObject("CombinedMeshes_" + combineParent.name);
+			foreach (var combinedObject in combinedObjects) combinedObject.transform.parent = resultGO.transform;
+		}
+		else {
+			resultGO = combinedObjects[0];
+		}
+
+		// Create prefab
+		Object prefab = PrefabUtility.CreateEmptyPrefab("Assets/" + resultGO.name + ".prefab");
+		PrefabUtility.ReplacePrefab(resultGO, prefab, ReplacePrefabOptions.ConnectToPrefab);
+
+		// Disable the original and return both to original positions
+		combineParent.SetActive(false);
+		combineParent.transform.position = originalPosition;
+		resultGO.transform.position = originalPosition;
+	}
 }
--- README.md
+++ README.md
@@ -1,15 +1,26 @@
 ## What does it do?
-Running the wizard will combine all the meshes on the chosen gameObject and its children that share the same material. If there is more than 1 material in all sub-objects, separate gameObjects will be created so that each object has one mesh with one material unique in the group, and will be parented under a new object. A prefab will be created from the combined gameObject in the root of the Assets folder, where all the newly created merged meshes will be created also. The original will be set inactive in the scene and the combined gameObject will be put in its position.
+Running the wizard will combine all the meshes on the chosen Game Object and its children that share the same material. If there is more than a single material in all sub-objects, sub-objects of result will be created so that each corresponds to a single material. A prefab will be created from the combined Game Object in the root of the Assets folder, with all the newly created merged meshes. The original will be set inactive in the scene and the combined Game Object will be put in its position.
 
-## How it works?
-Put the provided script in any Editor folder. In the top menu a new entry will appear ("E.S. Tools/Mesh Combine Wizard"). Picking this option will run the wizard. Pick the object from the scene that you wish to combine and clik _Create_.
+## How does it work?
+Put the provided script in any Editor folder. In the Menu Bar a new entry will appear ("E.S. Tools/Mesh Combine Wizard"). Picking this option will show the wizard dialog. The parent of objects to be combined should be assigned to a filed called _combine Parent_, which may be auto-assigned based on existing editor selection.
 
 ## Benefits:
 * Lowers the amount of draw calls, usually dramatically.
 * Does not need to draw all objects in the same batch regardless of wheter they are on screen or not, as opposed to static batching.
 * Does not compromise workflow - no need to merge modular pieces outside Unity in a 3D tool. Quick iteration time when changing the combined design.
-* Works with combined objects that have more than 64k verts, as opposed to solutions that manually set the verts and UVs.
-* Does not compromise the original object's pivot point, as opposed to similar solutions.
+* Works with combined objects that have more than 64k verts.
+* Does not compromise the original object's pivot point.
 
 ## Known limitations:
-* Does not support objects with multiple materials on submeshes. Such meshes should be split in an external 3D tool  to have separate meshes, 1 for each submesh and material pair of the original.
+* Does not support objects with multiple materials on submeshes. Such meshes should be split in an external 3D tool, so that there is only a single material per mesh.
+* Does not migrate components from the original other than MeshFilter and MeshRenderer.
+
+## Versions:
+2.0 - Added proper support for meshes with over 65K verts. Hardened the script and commented for public consumption.
+1.0 - Original release
+
+
+
+
+
+
